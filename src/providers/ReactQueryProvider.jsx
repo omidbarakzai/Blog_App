a91701b292c8cd267c467534ca35cf87cd330ff3
omidullah barakzai <135115963+omidbarakzai@users.noreply.github.com>
2025-03-19 19:18:35 +0330

    Update ReactQueryProvider.jsx

--- src/providers/ReactQueryProvider.jsx
+++ src/providers/ReactQueryProvider.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import { useState } from "react";
 
 const ReactQueryProvider = ({ children }) => {
@@ -10,7 +10,7 @@ const ReactQueryProvider = ({ children }) => {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
+   
     </QueryClientProvider>
   );
 };
